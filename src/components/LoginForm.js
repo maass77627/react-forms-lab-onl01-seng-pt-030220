4764783c27d834dec8903744bc04d91e76ed5880
kelly_maass <kmaass77627@gmail.com>
2020-12-06 17:48:34 -0600

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,45 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleInputChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+
+  }
+
+  handledLogin = event => {
+    event.preventDefault();
+    const username = this.state.username
+    const password = this.state.password
+    if (username !== "" && password !== "") {
+      this.props.handleLogin({username, password})
+      this.setState({
+        username: '',
+        password: ''
+      });
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handledLogin}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,34 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    // this.handleMaxChar = this.handleMaxChar.bind(this)
+    //console.log(this.props.maxChars)
+    this.state = {
+      char: " "
+    };
   }
+ 
+
+  handleMaxChar = event => {
+    console.log(event.target.value)
+    this.setState({
+      char: event.target.value
+    })
+  }
+
+  // handleSubmit = event => {
+  //   event.preventDefault()
+  // let formData = { char: this.state.char}
+  // //this.sendFormDataSomewhere(formData)
+  // }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={event => this.handleMaxChar(event)} value={this.state.char} />
+        <p>You have got {this.props.maxChars - this.state.char.length} characters remaining. </p>
+        
       </div>
     );
   }
